2f7376ee5b7e4cab18ad4ec83292a87a04f34ec0
AdityaVijay1 <adityavijay1712@gmail.com>
2024-01-04 14:04:31 +0800

    Version 1 Initial Changes

---
+++ footer.php
@@ -0,0 +1,7 @@
+<?php get_footer();?>
+<body>
+    <?php echo '<a href="https://www.jcu.edu.sg/openhouse">JCU Updates</a>'; ?> <br>
+    <?php echo 'Copyright © 2024 ';?><br>
+    <?php echo date("Y");?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Website Title</title>
+
+    <!-- Add your styles or link to your CSS file here -->
+    <link rel="stylesheet" href="style.css">
+
+    <?php wp_head(); ?> <!-- If using WordPress, include this line for plugins/scripts -->
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="wp-content/themes/newtheme/images/jculogo.png" height="500px", width="1000px">
+            </div>
+
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="/page.php">Page 1</a></li>
+                    <li><a href="/page.php">Page 2</a></li>
+                    
+                </ul>
+            </nav>
+        </div>
+    </header>
+
